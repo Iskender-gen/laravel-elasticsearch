8afc50998192d3e09f1cd8f713fa360a1056bbd9
Alexander Inyakin <iskender.gen@gmail.com>
2017-04-17 19:47:37 +0300

    configuration can now be set via .env file

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -34,7 +34,13 @@ return [
              */
 
             'hosts' => [
-                'localhost:9200',
+                [
+                    'host' => env('ELASTICSEARCH_HOST', 'localhost'),
+                    'port' => env('ELASTICSEARCH_PORT', 9200),
+                    'scheme' => env('ELASTICSEARCH_SCHEME', null),
+                    'user' => env('ELASTICSEARCH_USER', null),
+                    'pass' => env('ELASTICSEARCH_PASS', null),
+                ]
             ],
 
             /**
